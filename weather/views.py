ede72351635c36047f9994b78def80b0745aa3b4
Vadim <vadimdanilov88@yandex.ru>
2021-01-10 20:08:00 +0300

    api key added

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,13 @@
+import requests
 from django.shortcuts import render
 
 
+
 def index(request):
+	appid='6fa2cf01ef405b38aa17de1651d76437'
+	url='http://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+	city='London'
+	res=requests.get(url.format(city))
+	print(res.text)
 	return render(request, 'weather/index.html')
 
